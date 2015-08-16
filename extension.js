55ee7bb3011112701f057f286cf141baec6440d9
CoolBreese <BreesePlays1@gmail.com>
2015-08-16 10:33:24 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -99,7 +99,7 @@
       themeLink: null,
       fbLink: null,
       youtubeLink: null,
-      website: killion.co,
+      website: null,
       intervalMessages: [],
       messageInterval: 5,
       songstats: true,
